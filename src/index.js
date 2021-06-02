6dd65caece981dd73ba35b43e5fafc74f6664eda
tim819306 <t1220059@gmail.com>
2021-06-02 02:01:18 -0700

    #3

--- src/index.js
+++ src/index.js
@@ -3,12 +3,14 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
 import InputFile from './InputFile';
-
+import SortableTable from './SortableTable';
+import * as script from './Script.js';
 
 
 ReactDOM.render(
   <div>
     <InputFile accept=".xlsx, .xls" name ="匯入Excel" />
+    <SortableTable metadata={script.getMetadate()}/>
   </div>,
   document.getElementById('root')
 );
